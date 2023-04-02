163af2451fbfb150e0938eee8fa86231d19cfc78
My Name <i@example.com>
2023-04-02 16:39:24 +0300

    Step 1. Add header

---
+++ src/components/Header.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import {
+    Navbar,
+    Nav,
+    FormControl,
+    Container,
+    Form,
+    Button
+} from "react-bootstrap";
+
+import logo from "../static/logo192.png";
+
+export default class Header extends Component {
+    render() {
+        return (
+            <>
+            <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">
+                       <img
+                       src={logo}
+                       height="30"
+                       width="30"
+                       className="d-inline-block align-top"
+                       alt = "Logo"
+                       /> React Site
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-between">
+                        <Nav claccName="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/about">About us</Nav.Link>
+                            <Nav.Link href="/contacts">Contacts</Nav.Link>
+                            <Nav.Link href="/blog">Blog</Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <FormControl
+                                type ="text"
+                                placeholder="Search"
+                                className="me-sm-3"
+                            />
+                            <Button variant="outline-info">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            </>
+        )
+    }
+}
