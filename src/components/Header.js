381990d4ee855833ad4fe44c66fa10a3a7865916
My Name <i@example.com>
2023-04-02 17:43:13 +0300

    Step 3. Add carousel

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,7 +28,7 @@ export default class Header extends Component {
     render() {
         return (
             <Router>
-            <Navbar fixed="top" collapseOnSelect expand="md" className="d-block" bg="dark" variant="dark">
+            <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand href="/">
                        <img
@@ -41,7 +41,7 @@ export default class Header extends Component {
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                     <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-between">
-                        <Nav claccName="me-auto">
+                        <Nav className="me-auto">
                             <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="/about">About us</Nav.Link>
                             <Nav.Link href="/contacts">Contacts</Nav.Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import CarouselBoxHk from "../components/CarouselBoxHk"
 
 export default class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+      <CarouselBoxHk/>
     )
   }
 }
